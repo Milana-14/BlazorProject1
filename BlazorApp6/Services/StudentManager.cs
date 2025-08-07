fb6456d4bed8b91af567014f539b18cea75a5bc5
Milana <milanaulejfan@gmail.com>
2025-08-07 23:06:09 +0300

    Created the student filters

--- BlazorApp6/Services/StudentManager.cs
+++ BlazorApp6/Services/StudentManager.cs
@@ -1,10 +1,11 @@
 ﻿using BlazorApp6.Components.Models;
+using System.Text.Json;
 
 namespace BlazorApp6.Services
 {
     public class StudentManager
     {
-        public static List<Student> AddStudent(Student student) // Добавить проверку на повторения юзернейма
+        public static List<Student> AddStudent(Student student)
         {
             try
             {
@@ -44,6 +45,39 @@ namespace BlazorApp6.Services
             else
             {
                 throw new Exception("Ученик не найден");
+            }
+        }
+        public static List<Student> GetAllStudents()
+        {
+            return StudentFileManager.LoadFromFile();
+        }
+
+        // класс для чтения и записи данных в файл 
+        private static class StudentFileManager
+        {
+            public static void SaveToFile(List<Student> students)
+            {
+                string line = JsonSerializer.Serialize(students);
+                File.WriteAllText(AppConstants.StudentsFilePath, line);
+            }
+            public static List<Student> LoadFromFile()
+            {
+                if (!File.Exists(AppConstants.StudentsFilePath))
+                {
+                    return new List<Student>();
+                }
+
+                try
+                {
+                    string lines = File.ReadAllText(AppConstants.StudentsFilePath);
+                    List<Student> students = JsonSerializer.Deserialize<List<Student>>(lines) ?? new List<Student>();
+                    return students;
+                }
+                catch (Exception ex)
+                {
+                    // Можно логировать ошибку (если есть логгер), а пользователю показать:
+                    throw new ApplicationException("Зареждането на данните не беше успешно. Опитайте отново по-късно.");
+                }
             }
         }
     }
