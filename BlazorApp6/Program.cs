56f1663e38233dbe0c6db59cb262c3a2ef60ddc4
Milana <milanaulejfan@gmail.com>
2025-08-25 16:34:16 +0300

    Changed some services from Singleton to Scoped

--- BlazorApp6/Program.cs
+++ BlazorApp6/Program.cs
@@ -6,14 +6,11 @@ using System.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddScoped<IDbConnection>(sp => new SqliteConnection(connectionString));
-
 builder.Services.AddRazorComponents().AddInteractiveServerComponents();
 
 builder.Services.AddSingleton<AppState>();
-builder.Services.AddSingleton<StudentManager>();
-builder.Services.AddSingleton<MatchManager>();
+builder.Services.AddScoped<StudentManager>();
+builder.Services.AddScoped<MatchManager>();
 
 var app = builder.Build();
 
