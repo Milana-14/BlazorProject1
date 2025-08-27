c67a39057a215a04076e890b5883138d788c8686
Milana <milanaulejfan@gmail.com>
2025-08-27 11:48:57 +0300

    Removed signgs of working with files

--- BlazorApp6/Services/AppConstants.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BlazorApp6.Services
-{
-    public static class AppConstants // File paths
-    {
-        public const string MatchesFilePath = "Data/matches.json";
-        public const string HistoryFilePath = "Data/history.json";
-    }
-}
